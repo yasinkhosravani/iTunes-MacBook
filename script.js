8e374526f84e5938078e23170261d2dba683d533
yasin khosravani <31961300+yasinkhosravani@users.noreply.github.com>
2022-08-27 15:42:31 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+
+// import {music} from './js/music.js';
+function music(){
+    return[
+        {
+            name : "love Your voice",
+            singer : "jony",
+            link : "https://dl2.hitseda.com/dl1/russia/JONY-Love-your-voice.mp3?_=1",
+            img : "https://i1.sndcdn.com/artworks-ypbFcf8zydXCP2WN-l1vhyw-t500x500.jpg",
+            time : "2:30"
+        },
+        {
+            name : "you captured me",
+            singer : "jony",
+            link : "https://dl2.hitseda.com/dl1/russia/%D1%82%D1%8B-%D0%BC%D0%B5%D0%BD%D1%8F-%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%BB%D0%B0.mp3?_=2",
+            img : "https://f4.bcbits.com/img/a0726188235_16.jpg",
+            time : "3:25"
+        },
+        {
+            name : "childhood",
+            singer : "Rauf Faik",
+            link : "https://dl2.hitseda.com/dl1/russia/Rauf-Faik-Aetctbo.mp3?_=3",
+            img : "https://i1.sndcdn.com/artworks-000599214401-q5zx44-t500x500.jpg",
+            time : "3:08"
+        },
+        {
+            name : "leila",
+            singer : "jah Khalib",
+            link : "https://dl2.hitseda.com/dl1/russia/Leila-Jah-Khal%C3%ACb.mp3?_=4",
+            img : "https://i1.sndcdn.com/artworks-000194120998-iqnriv-t500x500.jpg",
+            time : "4:06"
+        },
+        {
+            name : "Девочка в тренде",
+            singer : "Miko",
+            link : "https://dl2.hitseda.com/dl1/russia/Miko-%D0%B4%D0%B5%D0%B2%D0%BE%D1%87%D0%BA%D0%B0-b-%D1%82%D1%80%D0%B5%D0%BD%D0%B4%D0%B5-1.mp3?_=7",
+            img : "https://text-pesni.com/public/img/songs/565671340/1.jpg?2",
+            time : "2:23"
+        }
+        
+    ]
+    }
+
+    
+    // console.log(music());
